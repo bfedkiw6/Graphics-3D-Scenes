bdeaf064a4d54c046ae89e91cb160a859fb1525c
bfedkiw6 <briana0nicole@gmail.com>
2023-10-22 13:28:16 -0700

    Updated

--- assignment1.objloader.js
+++ assignment1.objloader.js
@@ -20,7 +20,7 @@ class OBJLoader {
      * @returns {[Array<Number>, Array<Number>]} A tuple / list containing 1) the list of vertices and 2) the list of triangle indices
      */
     load() {
-        throw '"OBJLoader.load" not implemented' 
+    // throw '"OBJLoader.load" not implemented' 
 
         // Load the file's contents
         let contents = loadExternalFile(this.filename)
@@ -34,10 +34,34 @@ class OBJLoader {
         // You can loop through the file line-by-line by splitting the string at line breaks
         // contents.split('\n')
 
+        const lines = contents.split('\n')
+
         // TODO: STEP 2
         // Process (or skip) each line based on its content and call the parsing functions to parse an entry
         // For vertices call OBJLoader.parseVertex
         // For faces call OBJLoader.parseFace
+        
+        for (let i = 0; i < lines.length; i++) {
+            // Find if it is a vertex or index line
+            let elements = lines[i].split(' ')
+
+            // Parse for vertex
+            //console.log(elements[0])
+            if (elements[0] == 'v') {
+                let vert_vals = this.parseVertex(lines[i])
+                for (let j = 0; j < vert_vals.length; j++) {
+                    vertices.push(vert_vals[j])
+                }
+            } 
+            
+            // Parse for index
+            else if (elements[0] == 'f') {
+                let index_vals = this.parseFace(lines[i])
+                for (let k = 0; k < index_vals.length; k++) {
+                    indices.push(index_vals[k])
+                }
+            }
+        }
 
         // TODO: STEP 3
         // Vertex coordinates can be arbitrarily large or small
@@ -45,12 +69,42 @@ class OBJLoader {
         // As a pre-processing step and to avoid complicated scaling transformations in the main app we perform normalization here
         // Determine the max and min extent of all the vertex coordinates and normalize each entry based on this finding
 
+        //console.log(vertices)
+        vertices = this.normalizeVertices(vertices);
+       // console.log(vertices)
+
         // TODO: HINT
         // Look up the JavaScript functions String.split, parseFloat, and parseInt 
         // You will need thim in your parsing functions
 
         // Return the tuple
         return [ vertices, indices ]
+    }
+
+    /**
+     * Normalizes all vertex coordinates
+     * 
+     * @param {Array<Number>} vertex_list List containing vertex coordinates
+     * @returns {Array<Number>} A list containing normalized vertex coordinates
+     */
+    normalizeVertices(vertex_list)
+    {
+        // Find min and max
+        let min = 0
+        let max = 0
+        for (let j = 0; j < vertex_list.length; j++) {
+            if (vertex_list[j] < min)
+                min = vertex_list[j]
+            else if (vertex_list[j] > max)
+                max = vertex_list[j]
+        }
+     
+        // Loop through all vertex values and normalize with expression
+        for (let i = 0; i < vertex_list.length; i++) {
+            let new_val = 2 * ((vertex_list[i] - min)/(max-min)) - 1
+            vertex_list[i] = new_val
+        }
+        return vertex_list
     }
 
     /**
@@ -62,9 +116,17 @@ class OBJLoader {
      */
     parseVertex(vertex_string)
     {
-        throw '"OBJLoader.parseVertex" not implemented'
+    //throw '"OBJLoader.parseVertex" not implemented'
 
         // TODO: Process the entry and parse numbers to float
+        let v_vals = [];
+        let items = vertex_string.split(' ')
+        // Start a 1st number, ignore the 'v'
+        for (let i = 1; i < items.length; i++) {
+            v_vals.push(parseFloat(items[i]))
+        }
+
+        return v_vals;
     }
 
     /**
@@ -74,7 +136,7 @@ class OBJLoader {
      * This is done using OBJLoader.triangulateFace()
      * 
      * Each index entry can have up to three components separated by '/' 
-     * You need to grad the first component. The other ones are for textures and normals which will be treated later
+     * You need to grab the first component. The other ones are for textures and normals which will be treated later
      * Make sure to account for this fact.
      * 
      * Call this function from OBJLoader.load()
@@ -84,10 +146,25 @@ class OBJLoader {
      */
     parseFace(face_string)
     {
-        throw '"OBJLoader.parseFace" not implemented'
+    //throw '"OBJLoader.parseFace" not implemented'
 
         // TODO: Process the entry and parse numbers to ints
         // TODO: Don't forget to handle triangulation if quads are given
+        let f_vals = [];
+        let items = face_string.split(' ')
+
+        // Start a 1st number, ignore the 'f'
+        for (let i = 1; i < items.length; i++) {
+            // Subtract 1 to deal with indexing (1-based in file, must change to 0)
+            f_vals.push(parseInt(items[i]) - 1)
+        }
+        
+        // Quad face
+        if (f_vals.length == 4) {
+            f_vals = this.triangulateFace(f_vals)
+        }
+
+        return f_vals;
     }
 
     /**
@@ -100,9 +177,22 @@ class OBJLoader {
      */
     triangulateFace(face)
     {
-        throw '"OBJLoader.triangulateFace" not implemented'
+    //throw '"OBJLoader.triangulateFace" not implemented'
 
         // TODO: Triangulate the face indices
+        let new_face = []
+
+        // First Triangle
+        new_face.push(face[0])
+        new_face.push(face[1])
+        new_face.push(face[2])
+
+        // Second Triangle
+        new_face.push(face[0])
+        new_face.push(face[2])
+        new_face.push(face[3])
+
+        return new_face
     }
 }
 
